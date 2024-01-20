2bbfc6e294414b2300482af3771d5e287e53efa7
Phinpack <111883299+Phinpack@users.noreply.github.com>
2024-01-20 12:38:13 -0500

    Initial commit

---
+++ src/header.js
@@ -0,0 +1,16 @@
+// Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Metaverse Explorer</h1>
+      <div className="button-container">
+        <button>Button 1</button>
+        <button>Button 2</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
