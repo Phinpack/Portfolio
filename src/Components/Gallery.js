d988427d6a061079f514f208e1c873842f84635f
Phinpack <111883299+Phinpack@users.noreply.github.com>
2024-01-25 17:56:21 -0500

    Gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import '../CSS/Gallery.css'
+import clothing from '../images/ClothingStoreFromSecondFloor.png'
+import coaching from '../images/Coaching.png'
+import diner from '../images/DinerLeft.png'
+import zen from '../images/ZenEntrance.png'
 
 const Gallery = () => {
   return (
     <div className='gallery-container'>
-        <h1>Gallery</h1>
+        <h1 style={{ fontSize: '80px', maxWidth: '800px', fontWeight: '700'}}>Virtual Adventures Gallery</h1>
+        <div className='image-row-container'>
+            <div className='image-container'>
+                <img className='image' src={clothing}></img>
+            </div>
+            <div className='image-container'>
+                <img className='image' src={coaching}></img>
+            </div>
+            <div className='image-container'>
+                <img className='image' src={diner}></img>
+            </div>
+            <div className='image-container'>
+                <img className='image' src={zen}></img>
+            </div>
+            
+        </div>
     </div>
   )
 }
